96c72f5a5220502f895b43c5ae007a712daf0b8f
Abhi Vagadiya <your-email@example.com>
2025-12-05 16:39:39 +0530

    feat: add initial CSS styles and structure to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header.jsx'
+import './styles/main.scss'
 
 function App() {
 
+
   return (
-    <div>hii</div>
+
+    <div className="b">
+    <Header />
+    </div>
   )
 }
 
