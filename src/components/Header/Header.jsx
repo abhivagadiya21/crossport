0cea18048fb97d4e2a83923ac8dca69b6f1d9185
Abhi Vagadiya <your-email@example.com>
2025-12-05 18:14:17 +0530

    feat: add ethers dependency and implement wallet connection in Header and Connectwallet components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import crossport from '../../assets/cp.png'
-import wallet from '../../assets/wallet.svg'
+import React from "react";
+import crossport from "../../assets/cp.png";
+import wallet from "../../assets/wallet.svg";
+import { useAppKit, useAppKitAccount, useDisconnect } from "@reown/appkit/react";
 
 const Header = () => {
+  const { open } = useAppKit();
+  const { isConnected, address } = useAppKitAccount();
+  const { disconnect } = useDisconnect();
   return (
-    <>
-      <div className='header-main-container'>
-        <img  className='crossport-logo' src={crossport} />
-
-        <div className='connect-container'>
-            <div className='connect-wallet'>
-            <button className='connect-button'> <img className='wallet-icon' src={wallet} alt="" />
-            Connect Wallet</button>
-            </div>
+    
+    <div className="header-main-container">
+      <img className="crossport-logo" src={crossport} />
+      <div className="connect-container">
+        <div className="connect-wallet">
+          {isConnected ? (
+            <button className="connect-button" onClick={() => disconnect()} >
+              <img className="wallet-icon" src={wallet} /> 
+              {address.slice(0, 6)}...{address.slice(-4)}
+            </button>
+          ) : (
+            <button onClick={() => open()} className="connect-button">
+              <img className="wallet-icon" src={wallet} />
+              Connect Wallet
+            </button>
+          )}
+          
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
